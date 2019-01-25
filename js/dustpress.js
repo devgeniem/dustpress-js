39c28a4ffe9ecfab5b3617f26b5d483803543cb3
arttu <arttu.makiporhola@geniem.com>
2019-01-25 12:41:40 +0200

    changed dustpress.js to class format and added eslint configs

--- js/dustpress.js
+++ js/dustpress.js
@@ -1,173 +1,209 @@
-window.DustPress = ( function( window, document, $ ) {
-	var dp = {};
-	
-	dp.defaults = {
-		"type"	  		   : "post",
-		"tidy"    		   : false,
-		"render"  		   : false,
-		"partial" 		   : null,
-		"upload"  		   : false,
-		"data"             : false,
-		"url"              : null,
-		"bypassMainQuery"  : true,
-		"success" 		   : function() {},
-		"error"   		   : function() {},
-		"uploadProgress"   : function() {},
-		"downloadProgress" : function() {},
-		"contentType"      : "application/json"
-	};
+export default class DustPress {
 
-	dp.start    = function() {};
-	dp.complete = function() {};
+    /**
+     * Default instance params
+     *
+     * @type {Object}
+     */
+    defaults = {
+        get: '',
+        type: 'post',
+        tidy: false,
+        render: false,
+        partial: null,
+        upload: false,
+        data: false,
+        url: null,
+        bypassMainQuery: true,
+        contentType: 'application/json',
+        success: () => {},
+        error: () => {},
+        uploadProgress: () => {},
+        downloadProgress: () => {},
+        start: () => {},
+        complete: () => {}
+    };
 
-	// Create token for CSRF protection
-	if ( typeof window.crypto !== 'undefined' && typeof window.crypto.getRandomValues === 'function' ) { 
-		dp.token = '';
+    /**
+     * Constains current instance object
+     *
+     * @type {Object}
+     */
+    instance = {};
 
-		for ( var i = 0; i < 4; i++ ) {
-			dp.token += window.crypto.getRandomValues( new Uint32Array(1) );
-		}
-	}
-	else {
-		dp.token = Math.random() + '' + Math.random();
-	}
+    /**
+     * Construct class, setup crypt token
+     */
+    constructor() {
+        this.setupCrypto();
+    }
 
-	dp.ajax = function( path, params ) {
+    /**
+     * Setup token
+     */
+    setupCrypto() {
 
-		var post = $.extend( {}, dp.defaults, params );
+        // Create token for CSRF protection
+        if ( typeof window.crypto !== 'undefined' && typeof window.crypto.getRandomValues === 'function' ) {
+            this.defaults.token = '';
 
-		// Create a new instance of the default object so that simultaneous	calls wouldn't clash.
-		var instance = {};
+            for ( let i = 0; i < 4; i++ ) {
+                this.defaults.token += window.crypto.getRandomValues( new Uint32Array( 1 ) );
+            }
+        } else {
+            this.defaults.token = Math.random() + '' + Math.random();
+        }
+    }
 
-		// Assign the variables
-		instance.type               = post.type;
-		instance.success 	        = post.success;
-		instance.error 	            = post.error;
-		instance.url                = post.url;
-		instance.uploadProgress     = post.uploadProgress;
-		instance.downloadProgress   = post.downloadProgress;
-		instance.get 		        = post.get ? params.get : '';
-		instance.path		        = path;
-		instance.args               = post.args;
-		instance.data               = post.data;
-		instance.partial            = post.partial;
-		instance.render             = post.render;
-		instance.tidy               = post.tidy;
-		instance.upload             = post.upload;
-		instance.contentType        = post.contentType;
-		instance.bypassMainQuery    = post.bypassMainQuery;
-		instance.params 	        = params;
-		instance.start              = dp.start;
-		instance.complete           = dp.complete;
+    /**
+     * Handle ajax success
+     *
+     * @param  {string|mixed} data       Data retrieved by ajax call.
+     * @param  {string}       textStatus Response status.
+     * @param  {jqXHR}        jqXHR      Superset of the browser's native XMLHttpRequest object.
+     */
+    successHandler( data, textStatus, jqXHR ) {
+        let parsed;
 
-		if ( instance.get.length && ! instance.get.startsWith('?') ) {
-			instance.get = '?' + instance.get;
-		}
+        if ( typeof data === 'string' ) {
+            parsed = $.parseJSON( data );
+        } else {
+            parsed = data;
+        }
 
-		
+        // Expire CSRF cookie
+        document.cookie = 'dpjs_token=; expires=-1; path=/';
 
-		var date = new Date();
-        date.setTime( date.getTime() + ( 24*60*60*1000 ) );
-	    
-	    // Set the cookie for the token
-	    document.cookie = 'dpjs_token=' + dp.token + '; expires=' + date.toGMTString() + '; path=/';
+        // Add to debugger data if it exists
+        if ( window.DustPressDebugger ) {
+            this.addToDebugger( parsed );
+        }
 
-		var options = {
-			url: ( instance.url || dustpressjs_endpoint || ( window.location + instance.get ) ),
-			method: instance.type,
-			contentType: instance.contentType,
-			data: {
-				dustpress_data: {
-					path            : path,
-					args            : instance.args,
-					render          : instance.render,
-					tidy            : instance.tidy,
-					partial         : instance.partial,
-					data            : instance.data,
-					token           : dp.token,
-					bypassMainQuery : instance.bypassMainQuery
-				}
-			}
-		};
+        if ( parsed && ! parsed.error ) {
+            this.instance.success( parsed.success, parsed.data, textStatus, jqXHR );
+        } else {
+            this.instance.error( parsed, textStatus, jqXHR );
+        }
+    }
 
-		// Stringify data so it can be sent
-		options.data = JSON.stringify( options.data );
+    /**
+     * Add called data to dustpress debugger
+     *
+     * @param {object} parsed Parsed data.
+     */
+    addToDebugger( parsed ) {
+        delete this.instance.params.success;
+        delete this.instance.params.error;
 
-		if ( instance.upload ) {
-			options.xhr = function() {
-				var xhr = new window.XMLHttpRequest();
+        const debug = {
+            params: this.instance.params,
+            data: parsed.debug ? parsed.debug : parsed
+        };
+        window.DustPressDebugger.extend( debug, this.instance.path );
+    }
 
-				xhr.upload.addEventListener( 'progress', instance.uploadProgressHandler, false );
-				xhr.addEventListener( 'progress', instance.downloadProgressHandler, false );
-			};
-		}
+    /**
+     * Handle file uploads
+     */
+    xhrUpload() {
+        const xhr = new window.XMLHttpRequest();
 
-		instance.start();
+        xhr.upload.addEventListener( 'progress', ( event ) => this.instance.uploadProgressHandler( event ), false );
+        xhr.addEventListener( 'progress', ( event ) => this.instance.uploadDownloadProgressHandler( event ), false );
+    }
 
-		instance.successHandler = function( data, textStatus, jqXHR ) {
-			var parsed;
+    /**
+     * Handle errors.
+     *
+     * @param  {jqXHR}  jqXHR       Superset of the browser's native XMLHttpRequest object.
+     * @param  {string} textStatus  Response status.
+     * @param  {mixed}  errorThrown Ajax call error.
+     */
+    errorHandler( jqXHR, textStatus, errorThrown ) {
 
-			if ( typeof data === 'string' ) {
-				parsed = $.parseJSON( data );
-			}
-			else {
-				parsed = data;
-			}
-			
-			// Expire CSRF cookie
-			document.cookie = 'dpjs_token=; expires=-1; path=/';
-	
-			// Add to debugger data if it exists
-			if ( window.DustPressDebugger ) {
-				delete instance.params.success;
-				delete instance.params.error;
-	
-				var debug = {
-					params: instance.params,
-					data: parsed.debug ? parsed.debug : parsed
-				};
-				window.DustPressDebugger.extend(debug, instance.path);
-			}
-	
-			if ( parsed && ! parsed.error ) {
-				instance.success(parsed.success, parsed.data, textStatus, jqXHR);
-			}
-			else {
-				instance.error(parsed, textStatus, jqXHR);
-			}
-		};
-	
-		instance.errorHandler = function( jqXHR, textStatus, errorThrown ) {
-			// Expire CSRF cookie
-			document.cookie = 'dpjs_token=; expires=-1; path=/';
-	
-			instance.error({error: errorThrown}, textStatus, jqXHR);
-		};
-	
-		instance.uploadProgressHandler = function( event ) {
-			if ( event.lengthComputable ) {
-				var complete = ( event.loaded / event.total );
-	
-				instance.uploadProgress( complete );	
-			}
-		};
-	
-		instance.downloadProgressHandler = function( event ) {
-			if ( event.lengthComputable ) {
-				var complete = ( event.loaded / event.total );
-	
-				instance.downloadProgress( complete );
-			}
-		};
+        // Expire CSRF cookie
+        document.cookie = 'dpjs_token=; expires=-1; path=/';
 
-		return $.ajax( options )
-		.done( instance.successHandler )
-		.fail( instance.errorHandler )
-		.always( instance.complete );
-	};
+        this.instance.error({error: errorThrown}, textStatus, jqXHR );
+    }
 
-	return dp;
+    /**
+     * Handle uploar or download progress.
+     *
+     * @param {object} event Event data.
+     */
+    uploadDownloadProgressHandler( event ) {
+        if ( event.lengthComputable ) {
+            const complete = ( event.loaded / event.total );
 
-})( window, document, jQuery );
+            this.instance.downloadProgress( complete );
+        }
+    }
 
-window.dp = window.DustPress.ajax;
+    /**
+     * Make the ajax call.
+     *
+     * @param  {string}      path   Dustpress call path.
+     * @param  {object|null} params Ajax call params.
+     * @return {jqXHR}              Superset of the browser's native XMLHttpRequest object.
+     */
+    ajax( path, params ) {
+
+        // Create a new instance of the default object so that simultaneous calls wouldn't clash.
+        this.instance = Object.assign({}, this.defaults, params );
+        if ( this.instance.get.length && ! this.instance.get.startsWith( '?' ) ) {
+            this.instance.get = '?' + this.instance.get;
+        }
+
+        const date = new Date();
+        date.setTime( date.getTime() + ( 24 * 60 * 60 * 1000 ) );
+
+        // Set the cookie for the token
+        document.cookie = 'dpjs_token=' + this.instance.token + '; expires=' + date.toGMTString() + '; path=/';
+
+        const options = {
+            url: ( this.instance.url || dustpressjs_endpoint || ( window.location + this.instance.get ) ),
+            method: this.instance.type,
+            contentType: this.instance.contentType,
+            data: {
+                dustpress_data: {
+                    path: path,
+                    args: this.instance.args,
+                    render: this.instance.render,
+                    tidy: this.instance.tidy,
+                    partial: this.instance.partial,
+                    data: this.instance.data,
+                    token: this.instance.token,
+                    bypassMainQuery: this.instance.bypassMainQuery
+                }
+            }
+        };
+
+        // Stringify data so it can be sent
+        options.data = JSON.stringify( options.data );
+
+        if ( this.instance.upload ) {
+            options.xhr = () => this.xhrUpload();
+        }
+
+        this.instance.start();
+
+        this.instance.successHandler          = ( data, textStatus, jqXHR ) => this.successHandler( data, textStatus, jqXHR );
+        this.instance.errorHandler            = ( jqXHR, textStatus, errorThrown ) => this.errorHandler( jqXHR, textStatus, errorThrown );
+        this.instance.uploadProgressHandler   = ( event ) => this.uploadDownloadProgressHandler( event );
+        this.instance.downloadProgressHandler = ( event ) => this.uploadDownloadProgressHandler( event );
+
+        return $.ajax( options )
+        .done( ( data, textStatus, jqXHR ) => this.instance.successHandler( data, textStatus, jqXHR ) )
+        .fail( ( jqXHR, textStatus, errorThrown ) => this.instance.errorHandler( jqXHR, textStatus, errorThrown ) )
+        .always( ( dataOrXhr, textStatus, xhrOrError ) => this.instance.complete( dataOrXhr, textStatus, xhrOrError ) );
+    }
+}
+
+// Register legacy functionality
+if ( window.DustPress === 'undefined' ) {
+    window.DustPress = new DustPress();
+    window.dp = ( path, params ) => {
+        return window.DustPress.ajax( path, params );
+    };
+}
